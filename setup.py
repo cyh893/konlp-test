319e47147540af661b2f95eaa5aee49541f1f69b
cho <cyh905@nate.com>
2017-10-12 06:03:38 +0900

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+
 from distutils.core import setup, Extension
  
 setup(name = "konlp",
-        py_module=["konlp.detector.detector"],
+version = "1.0",
+        py_modules=["konlp.detector.detector"],
         ext_modules = [Extension("konlp.c.codescan", ["konlp/c_src/detector.c"]),
         ]
 )
