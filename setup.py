c64df34b26c8c124b46f128efbe7777928bb0d48
cho <cyh905@nate.com>
2017-10-29 23:27:48 +0900

    add ngram, wordcount

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
  
 setup(name = "konlp",
 version = "1.0",
-        py_modules=["konlp.detector.detector"],
+        py_modules=["konlp.detector.detector", "konlp.ngram.ngram"],
         ext_modules = [Extension("konlp.c.codescan", ["konlp/c_src/detector.c"]),
-        ]
+        Extension("konlp.wordcount", ["konlp/wordcount/wordcount.c", "konlp/wordcount/wordcount_.c", "konlp/wordcount/wordcount-main.c", "konlp/wordcount/wordcount-sort.c"])]
 )
